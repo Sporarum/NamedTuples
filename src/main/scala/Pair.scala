d1dad14fb3cb9381c6a3c48f3c260d5823431c73
Quentin Bernet <quentin.bernet@bluewin.ch>
2024-01-25 22:47:46 +0100

    Rewrite NamedPair as an opaque alias

--- src/main/scala/Pair.scala
+++ src/main/scala/Pair.scala
@@ -1,35 +1,38 @@
 import language.dynamics
 
-type LabelType = String & Singleton
+package Tuple:
 
-// Named pair constructor
-object P extends Dynamic:
-  transparent inline def applyDynamicNamed[LA <: LabelType, A, LB <: LabelType, B, T <: Tuple](inline s: "apply")(inline a: (LA, A), b: (LB, B)): Any =
-    NamedPair[LA, A, LB, B](a._2, b._2)
+  type LabelType = String & Singleton
 
-
-class NamedPair[LabelA <: LabelType, A, LabelB <: LabelType, B](val _1: A, val _2: B) extends Dynamic:
-
-  transparent inline def selectDynamic(inline s: String) =
-    inline s match
-      case _ : LabelA => _1
-      case _ : LabelB => _2
-      case _ =>
-        compiletime.error("Field does not exist")
+  // Named pair constructor
+  object P extends Dynamic:
+    transparent inline def applyDynamicNamed[LA <: LabelType, A, LB <: LabelType, B, T <: Tuple](inline s: "apply")(inline a: (LA, A), b: (LB, B)): Any =
+      (a._2, b._2).asInstanceOf[NamedPair[LA, A, LB, B]]
 
 
-given [LabelA <: LabelType, A, LabelB <: LabelType, B]: Conversion[(A, B), NamedPair[LabelA, A, LabelB, B]] = (a, b) => NamedPair(a, b)
+  opaque type NamedPair[LabelA <: LabelType, A, LabelB <: LabelType, B] <: Dynamic = (A, B) & Dynamic
 
-@main def testPair: Unit =
-  val john = P(age = 42, name = "John")
+  extension [LabelA <: LabelType, A, LabelB <: LabelType, B](p: NamedPair[LabelA, A, LabelB, B])
+    transparent inline def selectDynamic(inline s: String) =
+      inline s match
+        case _ : LabelA => p._1
+        case _ : LabelB => p._2
+        case _ =>
+          compiletime.error("Field does not exist")
 
-  john.age
-  john.name
 
-  //john.address // error: Field does not exist
+  given [LabelA <: LabelType, A, LabelB <: LabelType, B]: Conversion[(A, B), NamedPair[LabelA, A, LabelB, B]] = identity
 
-  type Person = NamedPair["age", Int, "name", String]
+  @main def testPair: Unit =
+    val john = P(age = 42, name = "John")
 
-  val bill: Person = (3, "Bill")
+    println(john.age)
+    println(john.name)
 
-  val john2: Person = john
+    //john.address // error: Field does not exist
+
+    type Person = NamedPair["age", Int, "name", String]
+
+    val bill: Person = (3, "Bill")
+
+    val john2: Person = john
